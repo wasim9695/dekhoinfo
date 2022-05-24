06f69dd969e1554092c40e7b7db6625a8a644c9a
zealts <wasim10786akram@gmail.com>
2022-05-24 10:10:13 +0530

    blog page

--- src/app/components/pages/blog/blog.component.ts
+++ src/app/components/pages/blog/blog.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { allModel } from '../../all-common/allModels';
+import {ServicesService} from '../../all-common/allServices';
+import {SubSink} from '../../all-common/Unsubscribe-adapter';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent extends SubSink  implements OnInit {
 
-  constructor() { }
+  public postData: any = new allModel();
+  constructor(private userService: ServicesService,
+    private router: Router) {super(); }
 
   ngOnInit(): void {
+   //alert(localStorage.getItem('currentUser'));
+   this.getAllPost();
+   
   }
+  getAllPost(){ 
+    this.userService.allPostList()
+       .subscribe(
+           data => {
+             this.postData=data;
+             //console.log(this.postData);
+             this.postData= data;
+             
+             console.log(this.postData);
+             
+             //this.getDataId();
+                //console.log(data);
+
+               //  started from data in this here
+
+              
+                 
+           },
+           error => {
+
+           });
+         }
+
+detailPage(Id){
+console.log(Id);
+//return false;
+localStorage.setItem("Id", Id);
+this.router.navigate(['/blog-details']);
+
+}
 
 }
